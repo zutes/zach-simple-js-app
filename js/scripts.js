cb41b2aa854244da921b94012a437792fa8a80ce
zutes <61748581+zutes@users.noreply.github.com>
2020-03-20 11:02:57 -0600

    Create new variable
    Created a variable named 'repository' that contains objects and arrays for Pokedex creatures.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello world');
-var favoriteFood = 'Greek Food';
-document.write(favoriteFood);
+var repository = [
+    {
+        name: 'Pikachu',
+        pokedexNumber: 25,
+        height: 0.4,
+        types: [electric],
+    },
+    {
+        name: 'Zapdos',
+        pokedexNumber: 145,
+        height: 1.6,
+        types: [electric, flying],
+    },
+    {
+        name: 'Dragonite',
+        pokedexNumber: 149,
+        height: 2.2,
+        types: [dragon, flying]
+    }
+];
