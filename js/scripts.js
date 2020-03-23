a8ff8b07eac7fc04efe279a386b67f86516d1d3e
zutes <61748581+zutes@users.noreply.github.com>
2020-03-23 13:19:52 -0600

    Created for loop
    Created a for loop that displays pokemon creature name and height. It also contains a conditional that outputs a specific string if height is >= 2.2.

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,25 @@ var repository = [
         types: ['electric'],
     },
     {
-        name: 'Zapdos',
-        pokedexNumber: 145,
-        height: 1.6,
-        types: ['electric', 'flying'],
-    },
-    {
         name: 'Dragonite',
         pokedexNumber: 149,
         height: 2.2,
         types: ['dragon', 'flying']
+    },
+    {
+        name: 'Zapdos',
+        pokedexNumber: 145,
+        height: 1.6,
+        types: ['electric', 'flying'],
     }
 ];
+
+for (var i = 0; i < repository.length; i++) {
+    
+        if(repository[i].height >= 2.2) {
+        document.write(repository[i].name + ' (height: ' + repository[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
+        }else{
+        document.write(repository[i].name + ' (height: ' + repository[i].height + ')' + '<br>');
+        
+    }
+}
