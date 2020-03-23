8454dc26e0398ac86b5fa9ccfab3e0dde412d9fe
zutes <61748581+zutes@users.noreply.github.com>
2020-03-23 08:05:13 -0600

    Added single quotes to array
    Added single quotes to the data in the "types" array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,18 @@ var repository = [
         name: 'Pikachu',
         pokedexNumber: 25,
         height: 0.4,
-        types: [electric],
+        types: ['electric'],
     },
     {
         name: 'Zapdos',
         pokedexNumber: 145,
         height: 1.6,
-        types: [electric, flying],
+        types: ['electric', 'flying'],
     },
     {
         name: 'Dragonite',
         pokedexNumber: 149,
         height: 2.2,
-        types: [dragon, flying]
+        types: ['dragon', 'flying']
     }
 ];
