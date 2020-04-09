fb3d26baa1197497481f626a5f15dfb59aacdd5b
zutes <61748581+zutes@users.noreply.github.com>
2020-04-09 13:55:56 -0600

    Format code
    Cleaned up the formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,54 @@
 
-var pokemonRepository = (function() {
-var repository = [
-    {
-        name: 'Pikachu',
-        pokedexNumber: 25,
-        height: 0.4,
-        types: ['electric'],
-    },
-    {
-        name: 'Dragonite',
-        pokedexNumber: 149,
-        height: 2.2,
-        types: ['dragon', 'flying']
-    },
-    {
-        name: 'Zapdos',
-        pokedexNumber: 145,
-        height: 1.6,
-        types: ['electric', 'flying'],
+var pokemonRepository = (function () {
+    var repository = [
+        {
+            name: 'Pikachu',
+            pokedexNumber: 25,
+            height: 0.4,
+            types: ['electric'],
+        },
+        {
+            name: 'Dragonite',
+            pokedexNumber: 149,
+            height: 2.2,
+            types: ['dragon', 'flying']
+        },
+        {
+            name: 'Zapdos',
+            pokedexNumber: 145,
+            height: 1.6,
+            types: ['electric', 'flying'],
+        }
+    ];
+
+    function add(pokemon) {
+        repository.push(pokemon);
     }
-];
 
-function add(pokemon) {
-    repository.push(pokemon);
-  }
+    function getAll() {
+        return repository;
+    }
 
-  function getAll() {
-    return repository;
-  }
+    function addListItem(pokemon) {
+        var $pokemonList = document.querySelector('ul');
+        var listItem = document.createElement('li');
+        var button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('list-button');
+        listItem.appendChild(button);
+        $pokemonList.appendChild(listItem);
+    };
 
-function addListItem(pokemon){
-    var $pokemonList = document.querySelector('ul');
-    var listItem = document.createElement('li');
-    var button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('list-button');
-    listItem.appendChild(button);
-    $pokemonList.appendChild(listItem);
-};
-
-
-
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem
-  };
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
+    };
 })();
-
-
-
-
 
 pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
-
-
 
 /*This is an Object.keys forEach loop
 
