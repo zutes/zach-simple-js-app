b0473961bc4cfba64386d7ce4eeb2b5c8caa7510
zutes <61748581+zutes@users.noreply.github.com>
2020-04-13 13:20:46 -0600

    API, promises, polyfills
    Add polyfills and promises.
    Remove array and create empty array.
    Add functions to get data from API.
    Add new comments and clean up comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,101 @@
-
+//Wrap repository array in an IIFE to avoid accidentally accessing the global state
 var pokemonRepository = (function () {
-    var repository = [
-        {
-            name: 'Pikachu',
-            pokedexNumber: 25,
-            height: 0.4,
-            types: ['electric'],
-        },
-        {
-            name: 'Dragonite',
-            pokedexNumber: 149,
-            height: 2.2,
-            types: ['dragon', 'flying']
-        },
-        {
-            name: 'Zapdos',
-            pokedexNumber: 145,
-            height: 1.6,
-            types: ['electric', 'flying'],
-        }
-    ];
-
+    //Creates an empty repository
+    var repository = [];
+    //Creates a variable to access the pokemon API
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    //Function to add each pokemon and attributes
     function add(pokemon) {
         repository.push(pokemon);
     }
 
+    //Function to pull all pokemon data
     function getAll() {
         return repository;
     }
 
+    //Function to create a list of items from API
     function addListItem(pokemon) {
+        //Assigns a variable to the ul list
         var $pokemonList = document.querySelector('ul');
+        //Assigns a variable to the list item
         var listItem = document.createElement('li');
+        //Assigns a variable to the button
         var button = document.createElement('button');
+        //Displays the current pokemon name on their button
         button.innerText = pokemon.name;
+        //Adds a CSS class to the button
         button.classList.add('list-button');
+        //Nests a button inside the list item
         listItem.appendChild(button);
+        //Nests list items inside a ul item
         $pokemonList.appendChild(listItem);
+        //Creates an event listener for when the button is clicked
         button.addEventListener('click', function (event) {
             showDetails(pokemon);
+        })
+    }
+
+    //Function to load pokemon list from API
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            // JSON used to exchange data back and forth with external servers
+            return response.json();
+            // If the promise is resolved, .then is run 
+                }).then(function (json) {
+            json.results.forEach(function (item) {
+                var pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+            // If the promise is rejected, .catch is run
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    // Function to load details for each pokemon:
+    function loadDetails(item) {
+        var url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // Adds the details to each item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = Object.keys(details.types);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+    // Function to return pokedex object array
+    function catchAll() {
+        return repository;
+    }
+
+    // Function to console.log pokemon details
+    function showDetails(item) {
+        pokemonRepository.loadDetails(item).then(function () {
+            console.log(item);
         });
     }
 
-    function showDetails(pokemon) {
-        console.log(pokemon.name + '\n' + pokemon.pokedexNumber + '\n' + pokemon.height + '\n' + pokemon.types);
-
-    }
-
-
+    // To return the values that can be accessed to outside the IIFE
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
         showDetails: showDetails
     };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+// To create list of pokemon with their name on the button
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-
-/*This is an Object.keys forEach loop
-
-Object.keys(repository).forEach(function (currentPokemon) {
-    document.write(repository[currentPokemon]);
-});
-*/
-
-
-/* This is a for loop
-
-for (var i = 0; i < repository.length; i++) {
-
-    if (repository[i].height >= 2.2) {
-        document.write(repository[i].name + ' (height: ' + repository[i].height + ')' + ' - Wow, that\'s big!' + '<br>');
-    } else {
-        document.write(repository[i].name + ' (height: ' + repository[i].height + ')' + '<br>');
-
-    }
-}
-*/
-
